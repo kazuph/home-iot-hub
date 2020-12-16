b0535d54311beabb5cc73c8c65f0974a1c1814f7
Borys Chylinski <borchy97@gmail.com>
2020-12-16 18:27:51 +0100

    [test] Add test control over serial port.

--- test/mqtt_test.py
+++ test/mqtt_test.py
@@ -1,4 +1,5 @@
 import unittest
+import serial
 from unittest import TestCase
 from secrets import token_hex
 from uuid import uuid4
@@ -21,8 +22,11 @@ class TestHomeIotHub(TestCase):
         self.port = config['BROKER_PORT']
         self.client = mqtt.Client(client_id=self.client_id)
         self.client.connect(host=self.host, port=self.port)
+        self.serial = serial.Serial('COM7', 74880, timeout=1)
+        self.serial.write(b'\xFF')
 
     def tearDown(self):
+        self.serial.close()
         self.client.disconnect()
 
     def test_wifi_connect_disconnect(self):
@@ -30,6 +34,8 @@ class TestHomeIotHub(TestCase):
         TEST WIFI CONNECT DISCONNECT
         """
         TEST_ID = 0
+        self.serial.write((TEST_ID).to_bytes(1, byteorder='big'))
+        self.serial.write(b'\n')
         pass
 
     def test_mqtt_echo(self):
@@ -38,23 +44,26 @@ class TestHomeIotHub(TestCase):
         Send 128 random tokens to client.
         """
         TEST_ID = 1
-        topic_publish = '/hub_test/publish'
-        topic_subscribe = '/hub_test/subscribe'
+        topic_publish = '/test/publish'
+        topic_subscribe = '/test/subscribe'
 
-        test_string = token_hex(128)
+        test_string = token_hex(4)
         TestHomeIotHub.reset_payload()
+
+        self.serial.write(TEST_ID.to_bytes(1, byteorder='big'))
+        self.serial.write(b'\n')
 
         self.client.subscribe(topic_subscribe)
         self.client.on_message = TestHomeIotHub.on_message    
         self.client.loop_start()
         self.client.publish(topic_publish, test_string)
-        time.sleep(1)
+        time.sleep(3)
         self.client.loop_stop()
         self.client.unsubscribe(topic_subscribe)
 
         with TestHomeIotHub.PAYLOAD_LOCK:
             self.assertIsNotNone(TestHomeIotHub.PAYLOAD)
-            self.assertEqual(test_string, str(TestHomeIotHub.PAYLOAD, 'utf-8'))
+            self.assertEqual(test_string, str(TestHomeIotHub.PAYLOAD[:len(test_string)], 'utf-8'))
 
     def test_mqtt_echo_repeated(self):
         """
@@ -62,25 +71,28 @@ class TestHomeIotHub(TestCase):
         Send 128 random tokens to client, repeat 10 times.
         """
         TEST_ID = 2
-        topic_publish = '/hub_test/publish'
-        topic_subscribe = '/hub_test/subscribe'
+        topic_publish = '/test/publish'
+        topic_subscribe = '/test/subscribe'
         reruns = 10
 
+        self.serial.write(TEST_ID.to_bytes(1, byteorder='big'))
+        self.serial.write(b'\n')
+
         for i in range(0, reruns):
-            test_string = token_hex(128)
+            test_string = token_hex(4)
             TestHomeIotHub.reset_payload()
 
             self.client.subscribe(topic_subscribe)
             self.client.on_message = TestHomeIotHub.on_message    
             self.client.loop_start()
             self.client.publish(topic_publish, test_string)
-            time.sleep(1)
+            time.sleep(3)
             self.client.loop_stop()
             self.client.unsubscribe(topic_subscribe)
 
             with TestHomeIotHub.PAYLOAD_LOCK:
                 self.assertIsNotNone(TestHomeIotHub.PAYLOAD)
-                self.assertEqual(test_string, str(TestHomeIotHub.PAYLOAD, 'utf-8'))
+                self.assertEqual(test_string, str(TestHomeIotHub.PAYLOAD[:len(test_string)], 'utf-8'))
 
     @staticmethod
     def on_message(client, userdata, message):
