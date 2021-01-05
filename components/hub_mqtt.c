0643cfac18d61678cd5e1abed1a61f1b6185d0ab
Borys Chylinski <borchy97@gmail.com>
2021-01-05 23:17:17 +0100

    [mqtt] Avoid use of function pointers in struct hub_mqtt_client.

--- components/hub_mqtt.c
+++ components/hub_mqtt.c
@@ -18,24 +18,36 @@ static const char *TAG = "HUB MQTT";
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) 
 {
     esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
-
-    // your_context_t *context = event->context;
+    
     switch (event->event_id) {
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA\n");
             hub_mqtt_client* client = (hub_mqtt_client*)handler_args;
+
+            if (client == NULL)
+            {
+                ESP_LOGE(TAG, "MQTT client not recognized.");
+                break;
+            }
+
+            if (client->_data_callback == NULL)
+            {
+                ESP_LOGE(TAG, "MQTT client data callback not valid.");
+                break;
+            }
+
             client->_data_callback(client, event->topic, event->data, event->data_len);           
             break;
         case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR\n");
+            ESP_LOGE(TAG, "MQTT_EVENT_ERROR\n");
             break;
         default:
-            ESP_LOGI(TAG, "Unknown event. ID: %d\n", event->event_id);
+            ESP_LOGW(TAG, "Unknown event. ID: %d\n", event->event_id);
             break;
     }
 }
 
-static esp_err_t _hub_mqtt_client_start(hub_mqtt_client* client)
+esp_err_t hub_mqtt_client_start(hub_mqtt_client* client)
 {
     esp_err_t result = ESP_OK;
 
@@ -63,12 +75,12 @@ static esp_err_t _hub_mqtt_client_start(hub_mqtt_client* client)
     return result;
 }
 
-static esp_err_t _hub_mqtt_client_stop(hub_mqtt_client* client)
+esp_err_t hub_mqtt_client_stop(hub_mqtt_client* client)
 {
     return esp_mqtt_client_stop(client->_client_handle);;
 }
 
-static esp_err_t _hub_mqtt_client_publish(hub_mqtt_client* client, const char* topic, const char* data)
+esp_err_t hub_mqtt_client_publish(hub_mqtt_client* client, const char* topic, const char* data)
 {
     int message_id = esp_mqtt_client_publish(client->_client_handle, topic, data, strlen(data), 1, 0);
     
@@ -82,7 +94,7 @@ static esp_err_t _hub_mqtt_client_publish(hub_mqtt_client* client, const char* t
     return ESP_OK;
 }
 
-static esp_err_t _hub_mqtt_client_subscribe(hub_mqtt_client* client, const char* topic)
+esp_err_t hub_mqtt_client_subscribe(hub_mqtt_client* client, const char* topic)
 {
     int message_id = esp_mqtt_client_subscribe(client->_client_handle, topic, 0);
 
@@ -96,7 +108,7 @@ static esp_err_t _hub_mqtt_client_subscribe(hub_mqtt_client* client, const char*
     return ESP_OK;
 }
 
-static esp_err_t _hub_mqtt_client_unsubscribe(hub_mqtt_client* client, const char* topic)
+esp_err_t hub_mqtt_client_unsubscribe(hub_mqtt_client* client, const char* topic)
 {
     int message_id = esp_mqtt_client_unsubscribe(client->_client_handle, topic);
 
@@ -110,7 +122,7 @@ static esp_err_t _hub_mqtt_client_unsubscribe(hub_mqtt_client* client, const cha
     return ESP_OK;
 }
 
-static esp_err_t _hub_mqtt_client_register_subscribe_callback(hub_mqtt_client* client, hub_mqtt_client_data_callback_t callback)
+esp_err_t hub_mqtt_client_register_subscribe_callback(hub_mqtt_client* client, hub_mqtt_client_data_callback_t callback)
 {
     client->_data_callback = callback;
     return ESP_OK;
@@ -118,14 +130,7 @@ static esp_err_t _hub_mqtt_client_register_subscribe_callback(hub_mqtt_client* c
 
 esp_err_t hub_mqtt_client_init(hub_mqtt_client* client, const hub_mqtt_client_config* config)
 {
-    client->start = &_hub_mqtt_client_start;
-    client->stop = &_hub_mqtt_client_stop;
-    client->publish = &_hub_mqtt_client_publish;
-    client->subscribe = &_hub_mqtt_client_subscribe;
-    client->unsubscribe = &_hub_mqtt_client_unsubscribe;
-    client->register_data_callback = &_hub_mqtt_client_register_subscribe_callback;
     client->_data_callback = NULL;
-
     client->_client_handle = esp_mqtt_client_init(config);
     if (client->_client_handle == NULL)
     {
@@ -140,14 +145,7 @@ esp_err_t hub_mqtt_client_destroy(hub_mqtt_client* client)
 {
     esp_err_t result = ESP_OK;
 
-    client->start = NULL;
-    client->stop = NULL;
-    client->publish = NULL;
-    client->subscribe = NULL;
-    client->unsubscribe = NULL;
-    client->register_data_callback = NULL;
     client->_data_callback = NULL;
-
     result = esp_mqtt_client_destroy(client->_client_handle);
     client->_client_handle = NULL;
 
--- components/include/hub_mqtt.h
+++ components/include/hub_mqtt.h
@@ -14,22 +14,26 @@ struct hub_mqtt_client
 {
     esp_mqtt_client_handle_t _client_handle;
     hub_mqtt_client_data_callback_t _data_callback;
-    esp_err_t (*start)(hub_mqtt_client* client);
-    esp_err_t (*stop)(hub_mqtt_client* client);
-    esp_err_t (*publish)(hub_mqtt_client* client, const char* topic, const char* data);
-    esp_err_t (*subscribe)(hub_mqtt_client* client, const char* topic);
-    esp_err_t (*unsubscribe)(hub_mqtt_client* client, const char* topic);
-    esp_err_t (*register_data_callback)(hub_mqtt_client* client, hub_mqtt_client_data_callback_t callback);
 };
 
-/*
-*   Initialize MQTT client with the specified config.
-*/
 esp_err_t hub_mqtt_client_init(hub_mqtt_client* client, const hub_mqtt_client_config* config);
 
-/*
-*   Destroy MQTT client.
-*/
+esp_err_t hub_mqtt_client_destroy(hub_mqtt_client* client);
+
+esp_err_t hub_mqtt_client_start(hub_mqtt_client* client);
+
+esp_err_t hub_mqtt_client_stop(hub_mqtt_client* client);
+
+esp_err_t hub_mqtt_client_publish(hub_mqtt_client* client, const char* topic, const char* data);
+
+esp_err_t hub_mqtt_client_subscribe(hub_mqtt_client* client, const char* topic);
+
+esp_err_t hub_mqtt_client_unsubscribe(hub_mqtt_client* client, const char* topic);
+
+esp_err_t hub_mqtt_client_register_subscribe_callback(hub_mqtt_client* client, hub_mqtt_client_data_callback_t callback);
+
+esp_err_t hub_mqtt_client_init(hub_mqtt_client* client, const hub_mqtt_client_config* config);
+
 esp_err_t hub_mqtt_client_destroy(hub_mqtt_client* client);
 
 #endif
