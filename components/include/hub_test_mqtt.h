0e010379af27e15836938d8f3abc3bc7045cdc8c
Borys Chylinski <borchy97@gmail.com>
2021-01-17 18:41:47 +0100

    [test] Remove test source and header files.

--- components/include/hub_test_mqtt.h
+++
@@ -1,8 +0,0 @@
-#ifndef HUB_TEST_MQTT_H
-#define HUB_TEST_MQTT_H
-
-#include "hub_test_utils.h"
-
-extern test_case test_mqtt_echo;
-
-#endif
--- components/include/hub_test_wifi.h
+++
@@ -1,8 +0,0 @@
-#ifndef HUB_TEST_WIFI_H
-#define HUB_TEST_WIFI_H
-
-#include "hub_test_utils.h"
-
-extern test_case test_wifi_connect_disconnect;
-
-#endif
