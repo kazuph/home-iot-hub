b7dc9220086596a1a3778c0482b65e03f9b5156c
Borys Chylinski <borchy97@gmail.com>
2021-01-18 12:42:15 +0100

    [ble] Implement is_device_supported function in device_list.

--- components/hub_device_list.c
+++ components/hub_device_list.c
@@ -1,4 +1,5 @@
 #include "hub_device_list.h"
+#include "string.h"
 
 #ifdef CONFIG_SUPPORT_XIAOMI_MI_KETTLE
 #include "device_mikettle.h"
@@ -7,14 +8,15 @@
 
 #endif
 
-const ble_device_type g_ble_supported_devices[] = {
+static const ble_device_type s_ble_supported_devices[] = {
 
 #ifdef CONFIG_SUPPORT_XIAOMI_MI_KETTLE
     {
-        .name = MIKETTLE_DEVICE_NAME,
+        .name = XIAOMI_MI_KETTLE_DEVICE_NAME,
         .connect = &mikettle_connect,
-        .notify = NULL,
-        .disconnect = NULL
+        .reconnect = &mikettle_reconnect,
+        .register_data_ready_callback = &mikettle_register_data_ready_callback,
+        .update_data = &mikettle_update_data
     },
 #endif
 
@@ -22,9 +24,36 @@ const ble_device_type g_ble_supported_devices[] = {
     {
         .name = NULL,
         .connect = NULL,
-        .notify = NULL,
-        .disconnect = NULL
+        .reconnect = NULL,
+        .register_data_ready_callback = NULL,
+        .update_data = NULL
     },
 #endif
 
 };
+
+const ble_device_type* get_type_for_device_name(const char* name)
+{
+    for (uint16_t i = 0; i < (uint16_t)(sizeof(s_ble_supported_devices) / sizeof(ble_device_type)); i++)
+    {
+        if (strcmp(name, s_ble_supported_devices[i].name) == 0)
+        {
+            return &s_ble_supported_devices[i];
+        }
+    }
+
+    return NULL;
+}
+
+bool is_device_supported(const char* name)
+{
+    for (uint16_t i = 0; i < (uint16_t)(sizeof(s_ble_supported_devices) / sizeof(ble_device_type)); i++)
+    {
+        if (strcmp(name, s_ble_supported_devices[i].name) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
--- components/include/hub_device_list.h
+++ components/include/hub_device_list.h
@@ -2,8 +2,49 @@
 #define HUB_DEVICE_LIST_H
 
 #include "hub_ble.h"
-#include "hub_ble_defines.h"
+#include "stdbool.h"
 
-extern const ble_device_type g_ble_supported_devices[];
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef esp_err_t (*connect_fun_t)(const hub_ble_client_handle_t client_handle, esp_bd_addr_t address, esp_ble_addr_type_t address_type);
+typedef esp_err_t (*reconnect_fun_t)(const hub_ble_client_handle_t client_handle);
+typedef void (*data_ready_callback_t)(const hub_ble_client_handle_t client_handle, const char* data, uint16_t data_length);
+typedef esp_err_t (*register_data_ready_callback_fun_t)(const hub_ble_client_handle_t client_handle, data_ready_callback_t data_ready_callback);
+typedef esp_err_t (*update_data_fun_t)(const hub_ble_client_handle_t client_handle, const char* data, uint16_t data_length);
+
+/*
+    Struct specifying BLE connect process and data
+    exchange function pointers for the given device type.
+
+    name - name of the device
+    connect - function allowing custom connection scheme for the given device
+    reconnect - reconnect function, handle is initialized by now, address and address type are memorized
+    register_data_ready_callback - register function that gets called whenever data is updated via BLE AND formatted as JSON string.
+    update_data - update_data device status request, the implementation is supposed to read JSON formated string and pass the data to BLE device.
+*/
+typedef struct ble_device_type
+{
+    const char* name;
+    connect_fun_t connect;
+    reconnect_fun_t reconnect;
+    register_data_ready_callback_fun_t register_data_ready_callback;
+    update_data_fun_t update_data;
+} ble_device_type;
+
+/*
+    Get pointer to the ble_device_type instance for the given device name.
+*/
+const ble_device_type* get_type_for_device_name(const char* name);
+
+/*
+    Check if device is suported.
+*/
+bool is_device_supported(const char* name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
