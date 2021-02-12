05613692f8c6d40046c9d6a6ea7886f56028fccc
Borys Chylinski <borchy97@gmail.com>
2021-02-12 19:11:15 +0100

    [devices] supported_devices changed to const instead of constexpr due to non-trivial destructors being used.

--- components/hub_device_factory.cpp
+++ components/hub_device_factory.cpp
@@ -10,18 +10,18 @@ namespace hub::device_factory
     static constexpr bool Is_device = std::is_base_of_v<device_base, _Ty>;
 
     template<typename _DeviceTy>
-    static std::enable_if_t<Is_device<_DeviceTy>, device_ptr_t> _make_device(device_id_t id)
+    static std::enable_if_t<Is_device<_DeviceTy>, std::unique_ptr<device_base>> _make_device(std::string_view id) noexcept
     {
         return std::make_unique<_DeviceTy>(id);
     }
 
     template<typename _DeviceTy>
-    static constexpr map_type::value_type register_device_type()
+    static constexpr std::pair<std::string_view, std::function<std::unique_ptr<device_base>(std::string_view)>> register_device_type() noexcept
     {
         return std::make_pair(_DeviceTy::device_name, _make_device<_DeviceTy>);
     }
 
-    static constexpr utils::const_map<std::string_view, std::function<std::unique_ptr<device_base>(std::string_view)>, 1> supported_devices{ {
+    static const utils::const_map<std::string_view, std::function<std::unique_ptr<device_base>(std::string_view)>, 1> supported_devices{ {
 #ifdef CONFIG_SUPPORT_XIAOMI_MI_KETTLE
         register_device_type<MiKettle>(),
 #endif
