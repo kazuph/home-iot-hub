6252b6eb27fdb0da0ebdc70b36a25b17f3a28b79
Borys Chylinski <borchy97@gmail.com>
2021-02-22 18:24:15 +0100

    [utils] Update dispatch_queue to handle calling functions with arguments.

--- components/include/hub_dispatch_queue.h
+++ components/include/hub_dispatch_queue.h
@@ -1,43 +1,33 @@
 #ifndef HUB_DISPATCH_QUEUE_H
 #define HUB_DISPATCH_QUEUE_H
 
-#include "hub_semaphore_lock.h"
+#include "hub_lock.h"
+#include "hub_mutex.h"
+#include "hub_task.h"
 
 #include "esp_log.h"
-#include "esp_err.h"
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "freertos/semphr.h"
 #include "freertos/event_groups.h"
 
 #include <functional>
 #include <queue>
+#include <tuple>
 #include <type_traits>
 #include <exception>
 #include <string>
+#include <utility>
 
 namespace hub::utils
 {
     template<
-        configSTACK_DEPTH_TYPE _Stack_size, 
-        UBaseType_t _Priority, 
-        typename _Ty = std::function<void(void)>, 
-        typename _QueueTy = std::queue<_Ty>>
-    class dispatch_queue : protected _QueueTy
+        typename _FunTy,
+        typename... _ArgTy>
+    class dispatch_queue : private std::queue<std::pair<_FunTy, std::tuple<_ArgTy...>>>
     {
-        static constexpr const char* TAG = "dispatch_queue";
-        static constexpr int QUEUE_EMPTY = BIT0;
-
-        using base = _QueueTy;
-
-        mutable volatile bool exit;
-        mutable TaskHandle_t task;
-        mutable SemaphoreHandle_t mutex;
-        mutable EventGroupHandle_t event_group;
-
     public:
+
+        using base = std::queue<std::pair<_FunTy, std::tuple<_ArgTy...>>>;
 
         using value_type      = typename base::value_type;
         using reference       = typename base::reference;
@@ -45,113 +35,75 @@ namespace hub::utils
         using size_type       = typename base::size_type;
         using container_type  = typename base::container_type;
 
-        static_assert(std::is_same_v<_Ty, value_type>, "dispatch_queue value type must match its container.");
+        using task_function_type    = std::function<void(void)>;
+        using task_internal_type    = task<task_function_type>;
 
-        dispatch_queue() : exit{ false }
+        dispatch_queue(configSTACK_DEPTH_TYPE stack_size = 2048, UBaseType_t priority = tskIDLE_PRIORITY) : 
+            _exit           { false }, 
+            _task           {  }, 
+            _mutex          {  }, 
+            _event_group    { xEventGroupCreate() }
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
 
             using namespace std::literals;
 
-            auto dispatch_fun = [](void* arg) -> void {
-                ESP_LOGD(TAG, "Function: %s.", __func__);
-
-                dispatch_queue* obj = reinterpret_cast<dispatch_queue*>(arg);
-                
-                while (!(obj->exit))
-                {
-                    if (!(obj->empty()))
-                    {
-                        if (obj->front())
-                        {
-                            (obj->front())();
-                        }
-
-                        obj->pop();
-                    }
-                    else
-                    {
-                        xEventGroupWaitBits(obj->event_group, dispatch_queue::QUEUE_EMPTY, pdTRUE, pdFALSE, portMAX_DELAY);
-                    }
-                }
-
-                vTaskDelete(nullptr);
-            };
-
-            mutex = xSemaphoreCreateRecursiveMutex();
-            if (mutex == nullptr)
+            if (_event_group == nullptr)
             {
-                ESP_LOGE(TAG, "Mutex create failed.");
                 abort();
             }
 
-            event_group = xEventGroupCreate();
-            if (event_group == nullptr)
-            {
-                ESP_LOGE(TAG, "Event group create failed.");
-                abort();
-            }
-
-            if (xTaskCreate(dispatch_fun, "dispatch_queue_task", _Stack_size, reinterpret_cast<void*>(this), _Priority, &task) != pdPASS)
-            {
-                ESP_LOGE(TAG, "Task create failed.");
-                abort();
-            }
+            _task = make_task<task_function_type>(stack_size, priority, [this]() { task_code(); });
         }
 
         ~dispatch_queue()
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-
-            exit = true;
-            xEventGroupSetBits(event_group, QUEUE_EMPTY);
-            {
-                semaphore_lock{ mutex };
-                vTaskDelete(task);
-                vEventGroupDelete(event_group);
-            }
-            vSemaphoreDelete(mutex);
+            lock<recursive_mutex> _lock{ _mutex };
+            _exit = true;
+            xEventGroupSetBits(_event_group, QUEUE_EMPTY);
+            vEventGroupDelete(_event_group);
         }
 
         bool empty() const
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::empty();
         }
 
         size_type size() const
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::size();
         }
 
-        reference& front()
+        reference front()
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::front();
         }
 
-        const_reference& front() const
+        const_reference front() const
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::front();
         }
 
-        reference& back()
+        reference back()
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::back();
         }
 
-        const_reference& back() const
+        const_reference back() const
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             return base::back();
         }
 
@@ -159,43 +111,90 @@ namespace hub::utils
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
             {
-                semaphore_lock{ mutex };
+                lock<recursive_mutex> _lock{ _mutex };
                 base::push(value);
             }
-            xEventGroupSetBits(event_group, QUEUE_EMPTY);
+            xEventGroupSetBits(_event_group, QUEUE_EMPTY);
         }
 
-        void push(value_type&& value)
+        template<typename... _Args>
+        void push(_FunTy&& fun, _Args&&... args)
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
             {
-                semaphore_lock{ mutex };
-                base::push(std::move(value));
+                lock<recursive_mutex> _lock{ _mutex };
+                base::push(
+                    std::make_pair<_FunTy, std::tuple<_Args...>>(
+                        std::forward<_FunTy>(fun), std::make_tuple(std::forward<_Args>(args)...)
+                    )
+                );
             }
-            xEventGroupSetBits(event_group, QUEUE_EMPTY);
+            xEventGroupSetBits(_event_group, QUEUE_EMPTY);
         }
 
-        template <class... Args> 
-        void emplace (Args&&... args)
+        template<typename... _Args>
+        void emplace(_FunTy&& fun, _Args&&... args)
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
-            base::emplace(std::forward(args...));
+            lock<recursive_mutex> _lock{ _mutex };
+            base::emplace(
+                std::make_pair<_FunTy, std::tuple<_Args...>>(
+                    std::forward<_FunTy>(fun), std::make_tuple(std::forward<_Args>(args)...)
+                )
+            );
         }
 
         void pop()
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             base::pop();
         }
 
         void swap(dispatch_queue& other) noexcept
         {
             ESP_LOGD(TAG, "Function: %s.", __func__);
-            semaphore_lock{ mutex };
+            lock<recursive_mutex> _lock{ _mutex };
             base::swap(other);
         }
+
+    private:
+
+        void task_code()
+        {
+            ESP_LOGD(TAG, "Function: main task (lambda).");
+            
+            while (true)
+            {
+                {
+                    lock<recursive_mutex> _lock{ _mutex };
+                    if (_exit)
+                    {
+                        return;
+                    }
+                }
+
+                if (!empty())
+                {
+                    const auto& [fun, args] = front();
+                    std::apply(fun, args);
+
+                    pop();
+                }
+                else
+                {
+                    xEventGroupWaitBits(_event_group, QUEUE_EMPTY, pdTRUE, pdFALSE, portMAX_DELAY);
+                }
+            }
+        }
+
+        static constexpr const char* TAG    { "dispatch_queue" };
+        static constexpr int QUEUE_EMPTY    { BIT0 };
+
+        mutable volatile bool       _exit;
+        mutable task_internal_type  _task;
+        mutable recursive_mutex     _mutex;
+        mutable EventGroupHandle_t  _event_group;
     };
 }
 
