d87d9aafec823842e3317218ab6c426d54b6d292
Borys Chylinski <borchy97@gmail.com>
2021-03-21 20:53:46 +0100

    [hub] Create asynchronous event system.

---
+++ components/include/hub_event.h
@@ -0,0 +1,65 @@
+#ifndef HUB_EVENT_H
+#define HUB_EVENT_H
+
+#include "esp_log.h"
+
+#include <functional>
+#include <algorithm>
+#include <list>
+#include <any>
+
+#include "hub_dispatch_queue.h"
+
+namespace hub
+{
+    struct dispatcher
+    {
+        static constexpr const char* TAG{ "DISPATCHER" };
+        static utils::dispatch_queue dispatch_queue;
+
+        template<typename Fun, typename... Args>
+        void dispatch(Fun&& fun, Args&&... args)
+        {
+            ESP_LOGD(TAG, "Function: %s.", __func__);
+            dispatch_queue.push(std::forward<Fun>(fun), std::forward<Args>(args)...);
+        }
+    };
+
+    template<typename EventArgsT>
+    class event_handler : protected dispatcher
+    {
+    public:
+
+        using function_type         = std::function<void(EventArgsT)>;
+        using function_list_type    = std::list<function_type>;
+
+        void invoke(EventArgsT args)
+        {
+            ESP_LOGD(TAG, "Function: %s.", __func__);
+
+            std::for_each(callback_list.begin(), callback_list.end(), [this, args](const auto& fun) {
+                dispatch(fun, args);
+            });
+        }
+
+        void operator+=(function_type fun)
+        {
+            ESP_LOGD(TAG, "Function: %s.", __func__);
+            callback_list.push_back(std::forward<function_type>(fun));
+        }
+
+        operator bool() const
+        {
+            ESP_LOGD(TAG, "Function: %s.", __func__);
+            return !callback_list.empty();
+        }
+
+    private:
+
+        static constexpr const char* TAG { "EVENT HANDLER" };
+
+        function_list_type callback_list;
+    };
+}
+
+#endif
