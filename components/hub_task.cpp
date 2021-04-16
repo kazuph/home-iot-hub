6271a17b4d772d23e35324d913983561b57332d8
Borys Chylinski <borchy97@gmail.com>
2021-04-16 17:21:07 +0200

    [refactor] Move task to hub::concurrency namespace. Add task indexing. Remove TASK_STARTED_BIT constant, use m_event_group validity instead.

--- components/hub_task.cpp
+++ components/hub_task.cpp
@@ -1,74 +1,59 @@
 #include "hub_task.h"
 
-namespace hub::utils
+namespace hub::concurrency
 {
+    uint32_t task::s_task_counter{ 0U };
+
     task::task() noexcept :
-        task_handle     { nullptr }, 
-        event_group     { xEventGroupCreate() }
+        m_task_handle{ nullptr }, 
+        m_event_group{ nullptr }
     {
-        ESP_LOGD(TAG, "Function: %s.", __func__);
+
     }
 
     task::task(task&& other) noexcept :
-        task_handle     { nullptr },
-        event_group     { xEventGroupCreate() }
+        m_task_handle{ nullptr },
+        m_event_group{ nullptr }
     {
-        ESP_LOGD(TAG, "Function: %s (move constructor).", __func__);
         *this = std::move(other);
     }
 
     task& task::operator=(task&& other)
     {
-        ESP_LOGD(TAG, "Function: %s (move assignment).", __func__);
         if (this == &other)
         {
             return *this;
         }
 
-        task_handle = other.task_handle;
-        event_group = other.event_group;
+        m_task_handle = other.m_task_handle;
+        m_event_group = other.m_event_group;
 
-        other.task_handle = nullptr;
-        other.event_group = nullptr;
+        other.m_task_handle = nullptr;
+        other.m_event_group = nullptr;
 
         return *this;
     }
 
-    bool task::joinable() const
-    {
-        ESP_LOGD(TAG, "Function: %s.", __func__);
-
-        // TASK_STARTED_BIT gets cleared in join() function.
-        return ((TASK_STARTED_BIT & xEventGroupGetBits(event_group)) && event_group);
-    }
-
     void task::join()
     {
-        ESP_LOGD(TAG, "Function: %s.", __func__);
+        assert(m_event_group);
 
-        if (!joinable())
+        EventBits_t bits = xEventGroupWaitBits(m_event_group, TASK_FINISHED_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
+
+        if (!(bits & TASK_FINISHED_BIT))
         {
-            return;
+            throw std::runtime_error("Task join timeout.");
         }
 
-        {
-            EventBits_t bits = xEventGroupWaitBits(event_group, TASK_STARTED_BIT | TASK_FINISHED_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
-
-            if (!(bits & (TASK_STARTED_BIT | TASK_FINISHED_BIT)))
-            {
-                ESP_LOGE(TAG, "Task join timeout.");
-                throw std::runtime_error("Task join timeout.");
-            }
-        }
+        vEventGroupDelete(m_event_group);
+        m_event_group = nullptr;
     }
 
     task::~task()
     {
-        if (!event_group)
+        if (joinable())
         {
-            return;
+            abort();
         }
-
-        vEventGroupDelete(event_group);
     }
 }
--- components/include/hub_task.h
+++ components/include/hub_task.h
@@ -1,23 +1,29 @@
 #ifndef HUB_TASK_H
 #define HUB_TASK_H
 
+#ifndef configUSE_TIME_SLICING
 #define configUSE_TIME_SLICING 1
+#endif
+
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-
-#include "esp_log.h"
 
 #include <functional>
 #include <tuple>
 #include <memory>
 #include <exception>
 #include <stdexcept>
+#include <new>
 
 #include "hub_timing.h"
 
-namespace hub::utils
+namespace hub::concurrency
 {
+    /**
+     * @brief Class wrapper for FreeRTOS task.
+     */
     class task
     {
     public:
@@ -28,68 +34,89 @@ namespace hub::utils
 
         task(task&& other) noexcept;
 
+        /**
+         * @brief Constructor creating and starting a new task.
+         * 
+         * @tparam FunTy Type of the function to be executed.
+         * @tparam Args Types of the arguments passed to the executed function.
+         * @param stack_size Stack size described in words to be allocated for task execution.
+         * @param priority Priority of the task.
+         * @param fun Function to be executed in task.
+         * @param args Arguments passed tu the function being executed in task.
+         */
         template<typename FunTy, typename... Args>
         task(configSTACK_DEPTH_TYPE stack_size, UBaseType_t priority, FunTy&& fun, Args&&... args) : 
-            task_handle     { nullptr },
-            event_group     { xEventGroupCreate() }
+            m_task_handle{ nullptr },
+            m_event_group{ xEventGroupCreate() }
         {
-            ESP_LOGD(TAG, "Function: %s.", __func__);
+            using namespace std::literals;
 
             struct param_t
             {
                 FunTy                   fun;
                 std::tuple<Args...>     args;
-                EventGroupHandle_t      event_group;
+                EventGroupHandle_t      m_event_group;
             };
 
-            if (!event_group)
+            if (!m_event_group)
             {
-                ESP_LOGE(TAG, "Event group was nullptr.");
-                throw std::runtime_error("Event group was nullptr.");
+                throw std::bad_alloc();
             }
 
             auto task_code = [](void* param) {
-                ESP_LOGD(TAG, "Function: task_code (lambda).");
                 {
                     std::unique_ptr<param_t> param_ptr{ reinterpret_cast<param_t*>(param) };          
-                    xEventGroupSetBits(param_ptr->event_group, TASK_STARTED_BIT);
                     std::apply(param_ptr->fun, param_ptr->args);
-                    xEventGroupSetBits(param_ptr->event_group, TASK_FINISHED_BIT);
+                    xEventGroupSetBits(param_ptr->m_event_group, TASK_FINISHED_BIT);
                 }
                 vTaskDelete(nullptr);
             };
 
             {
-                std::unique_ptr<param_t> param_ptr{ new param_t{ std::forward<FunTy>(fun), std::make_tuple(std::forward<Args>(args)...), event_group } };
+                std::unique_ptr<param_t> param_ptr{ new param_t{ std::forward<FunTy>(fun), std::make_tuple(std::forward<Args>(args)...), m_event_group } };
 
-                if (xTaskCreate(task_code, "task", stack_size, reinterpret_cast<void*>(param_ptr.get()), priority, &task_handle) != pdPASS)
+                if (xTaskCreate(task_code, ("task_"s + std::to_string(s_task_counter)).c_str(), stack_size, reinterpret_cast<void*>(param_ptr.get()), priority, &m_task_handle) != pdPASS)
                 {
-                    ESP_LOGE(TAG, "Task creation failed.");
                     throw std::runtime_error("Task creation failed.");
                 }
 
+                // When the task has started correctly, the allocated memory gets released inside the task code
                 param_ptr.release();
             }
+
+            s_task_counter++;
         }
 
         task& operator=(const task&) = delete;
 
         task& operator=(task&& other);
 
-        bool joinable() const;
+        /**
+         * @brief Check whether the task is in joinable state. 
+         * 
+         * @return true when the task has not yet been joined.
+         * @return false otherwise.
+         */
+        bool joinable() const
+        {
+            return (m_event_group);
+        }
 
+        /**
+         * @brief Synchronously wait for the task to finish its execution.
+         */
         void join();
 
         ~task();
 
     private:
 
-        static constexpr const char* TAG                { "TASK" };
-        static constexpr EventBits_t TASK_STARTED_BIT   { BIT0 };
-        static constexpr EventBits_t TASK_FINISHED_BIT  { BIT1 };
+        static constexpr EventBits_t    TASK_FINISHED_BIT  { BIT0 };
 
-        mutable TaskHandle_t        task_handle;
-        mutable EventGroupHandle_t  event_group;
+        static uint32_t                 s_task_counter;
+
+        TaskHandle_t                    m_task_handle;
+        EventGroupHandle_t              m_event_group;
     };
 }
 
