878e22f4acef2901fd6435dc3e07b99e688a85d3
Borys Chylinski <borchy97@gmail.com>
2021-06-12 22:46:37 +0200

    [utils] Implement dump function for JSON.

--- components/hub-utils/include/utils/json.hpp
+++ components/hub-utils/include/utils/json.hpp
@@ -1,10 +1,25 @@
 #ifndef HUB_UTILS_JSON_HPP
 #define HUB_UTILS_JSON_HPP
 
+#include <string>
+
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/istreamwrapper.h"
+
+namespace hub::utils::json
+{
+    using namespace rapidjson;
+
+    inline std::string dump(Document&& document) noexcept
+    {
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+        document.Accept(writer);
+        return std::string(buffer.GetString(), buffer.GetSize());
+    }
+}
 
 #endif
