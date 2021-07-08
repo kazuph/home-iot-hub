3f1f5e61d8792ad45762fb1e7facddc03f8e572d
Borys Chylinski <borchy97@gmail.com>
2021-07-08 23:06:17 +0200

    [utils] Create basic JSON utility functions.

--- components/hub-utils/include/utils/json.hpp
+++ components/hub-utils/include/utils/json.hpp
@@ -1,7 +1,10 @@
 #ifndef HUB_UTILS_JSON_HPP
 #define HUB_UTILS_JSON_HPP
 
+#include <cassert>
 #include <string>
+#include <string_view>
+#include <fstream>
 
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
@@ -11,14 +14,64 @@
 
 namespace hub::utils::json
 {
-    using namespace rapidjson;
-
-    inline std::string dump(Document&& document) noexcept
+    inline std::string dump(rapidjson::Document&& document) noexcept
     {
         rapidjson::StringBuffer buffer;
         rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
         document.Accept(writer);
         return std::string(buffer.GetString(), buffer.GetSize());
+    }
+
+    inline std::string dump(const rapidjson::Document& document) noexcept
+    {
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+        document.Accept(writer);
+        return std::string(buffer.GetString(), buffer.GetSize());
+    }
+
+    inline rapidjson::Document parse_file(std::string_view path) noexcept
+    {
+        rapidjson::Document file;
+        std::ifstream ifs(path.data());
+
+        if (!ifs)
+        {
+            return file;
+        }
+
+        {
+            rapidjson::IStreamWrapper json_stream(ifs);
+            file.ParseStream(json_stream);
+        }
+
+        return file;
+    }
+
+    inline bool has_parse_error(const rapidjson::Document& document) noexcept
+    {
+        return document.HasParseError();
+    }
+
+    inline bool is_object(const rapidjson::Document& document) noexcept
+    {
+        return document.IsObject();
+    }
+
+    inline bool has_member(const rapidjson::Document& document, std::string_view key) noexcept
+    {
+        assert(is_object(document));
+        return document.HasMember(key.data());
+    }
+
+    inline auto has_member_predicate(std::string_view key) noexcept
+    {
+        return [=](const rapidjson::Document& document) { return has_member(document, key); };
+    }
+
+    inline bool is_array(const rapidjson::Document& document) noexcept
+    {
+        return document.IsArray();
     }
 }
 
