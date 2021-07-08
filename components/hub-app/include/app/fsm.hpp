c8c62d94f9d6f481fa1ace41933b71861b92d45c
Borys Chylinski <borchy97@gmail.com>
2021-07-08 23:08:47 +0200

    [app] Implement state machine for application state representation.

--- components/hub-app/include/app/fsm.hpp
+++ components/hub-app/include/app/fsm.hpp
@@ -5,8 +5,23 @@
 #include <variant>
 #include <cassert>
 
+#ifndef HUB_FSM_NOEXCEPT
+    #ifndef NO_EXCEPTIONS
+        #define HUB_FSM_NOEXCEPT
+    #else
+        #define HUB_FSM_NOEXCEPT noexcept
+    #endif
+#endif
+
 namespace hub
 {
+    struct state_check {};
+
+    struct no_state_check {};
+
+    template<typename StateCheckT>
+    inline constexpr bool is_state_check_type_v = std::is_same_v<StateCheckT, state_check> || std::is_same_v<StateCheckT, no_state_check>;
+
     template<typename... StatesT>
     class fsm
     {
@@ -40,24 +55,45 @@ namespace hub
         }
 
         template<typename StateT, typename... ArgsT>
-        void set_state(ArgsT&&... args) noexcept
+        StateT& set_state(ArgsT&&... args) noexcept
         {
             static_assert(is_state_type_v<StateT>, "Not a valid state type.");
             static_assert(std::is_constructible_v<StateT, ArgsT...>, "State is not constructible with supplied arguments.");
             m_state = StateT(std::forward<ArgsT>(args)...);
+            return std::get<StateT>(m_state);
         }
 
-        template<typename StateT>
-        StateT& get_state() noexcept
+        template<typename StateT, typename StateCheckT = state_check>
+        StateT& get_state() HUB_FSM_NOEXCEPT
         {
             static_assert(is_state_type_v<StateT>, "Not a valid state type.");
-            assert(is_state<StateT>());
+            static_assert(is_state_check_type_v<StateCheckT>, "Available options for state check are: state_check and no_state_check.");
+
+            if constexpr (std::is_same_v<StateCheckT, state_check>)
+            {
+                throw_on_invalid_state<StateT>();
+            }
+
             return std::get<StateT>(m_state);
         }
 
     private:
 
         state_t m_state;
+
+        template<typename StateT>
+        void throw_on_invalid_state() HUB_FSM_NOEXCEPT
+        {
+            static_assert(is_state_type_v<StateT>, "Not a valid state type.");
+            if (!is_state<StateT>())
+            {
+#ifndef NO_EXCEPTIONS
+                throw std::logic_error("Invalid state.");
+#else
+                abort();
+#endif
+            } 
+        }
     };
 }
 
